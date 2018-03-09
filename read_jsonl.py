cf08232677fa6bc7ef3bbeae24d28b2bda4db9c1
HayatoTsukiji <hayatotsukiji@gmail.com>
2018-03-09 21:02:17 +0900

    add testdata,read_jsonl.py
    読み込みプログラムとテストデータの追加

---
+++ read_jsonl.py
@@ -0,0 +1,19 @@
+import jsonlines
+
+with jsonlines.open('0168.jsonl') as reader:
+    for files in reader:
+        tags = files["tags"]
+        video_id = files["video_id"]
+        watch_num = files["watch_num"]
+        comment_num = files["comment_num"]
+        mylist_num = files["mylist_num"]
+        title = files["title"]
+        description = files["description"]
+        category = files["category"]
+        tag_index = files["tags"]
+        upload_time = files["upload_time"]
+        file_type = files["file_type"]
+        length = files["length"]
+        size_high = files["size_high"]
+        size_low = files["size_low"]
+        print(tags)
