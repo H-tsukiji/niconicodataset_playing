31c8ae7b1bf2c67932258696dfb22a35c681aafa
HayatoTsukiji <hayatotsukiji@gmail.com>
2018-03-13 15:38:57 +0900

    カテゴリにおける動画データ抽出プログラムの追加

---
+++ extract_moviedata.py
@@ -0,0 +1,12 @@
+import jsonlines
+
+search_category = "VOCALOID"
+read_file = "../niconico_moviedata.jsonl"
+write_file = "../vocaloid_moviedata.jsonl"
+
+with jsonlines.open(read_file) as reader:
+    for files in reader:
+        category = files["category"]
+        if category == search_category:
+            with jsonlines.open(write_file, mode='a') as writer:
+                writer.write(files)
