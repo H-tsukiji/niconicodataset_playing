a1b2693752c90764028ffe9e3f78aecffcd10bbf
HayatoTsukiji <hayatotsukiji@gmail.com>
2018-03-16 18:48:19 +0900

    tagベクトル作成プログラムの作成(処理激重)

--- make_tagvector.py
+++ make_tagvector.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-import jsonlines
+import jsonlines,csv
 import numpy as np
 
 read_file = "../test_data.jsonl"
@@ -7,7 +7,7 @@ tag_index = []
 count = 1
 
 #タグインデックス読み込み
-f = open("testread.txt","r")
+f = open("testread.txt","r",encoding="utf-8")
 for line in f:
     line = line.replace('\n','')
     tag_index.append(line)
@@ -20,17 +20,29 @@ with jsonlines.open(read_file) as reader:
         movie_tag = np.zeros(len(tag_index))
         for tag in files["tags"]:
             if tag in tag_index:
-                continue
+                index_num = tag_index.index(tag)
+                movie_tag[index_num] = 1
             else:
-                tag_index.append(tag)
+                continue
+        tag_vac = np.vstack((tag_vac,movie_tag))
         count = count + 1
+tag_vac = np.delete(tag_vac,0,0)
+
+
+print(tag_vac)
+
+'''
+write_file = open("../testvsctor.csv","w",encoding="utf-8")
+for line in tag_index:
+    for score in tag_index[line]:
+        write_file.write(str(score)+",")
+    write_file.write("\n")
+'''
 
 '''
 hoge = [0,0,1,1,1]
 ho = np.array(hoge)
 print(hoge)
-
-
 ## numpy.vstack() で行方向に結合
 >>> np.vstack((a,b))
 array([[0, 1, 2],
